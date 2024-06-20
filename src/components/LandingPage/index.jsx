a26e0ee0920e4551c505eb37d562b06a1c3baab4
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-06-20 23:35:06 +0530

    hero page updated

--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -9,8 +9,8 @@ export const Hero = () => {
         <div className='hero'>
             <div className='hero-text'>
                 <motion.h2
-                    initial={{ opacity: 0, scale: 0.9, y: 50 }}
-                    animate={{ opacity: 1, scale: 1, y: 0 }}
+                    initial={{ opacity: 0,  y: 50 }}
+                    animate={{ opacity: 1,  y: 0 }}
                     transition={{ duration: 0.5 }}
                 >
                     Crypto Monitoring
@@ -79,6 +79,13 @@ export const Hero = () => {
             ></motion.div>
             <motion.div
                 className='purple-gradient3'
+                initial={{ opacity: 0, x: 450 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 1 }}
+
+            ></motion.div>
+            <motion.div
+                className='purple-gradient4'
                 initial={{ opacity: 0, x: 450 }}
                 animate={{ opacity: 1, x: 0 }}
                 transition={{ duration: 1 }}
